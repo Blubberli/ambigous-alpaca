997b68c154f78aa1cfa0568348bac512e8be5c4f
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-01-17 16:28:34 +0100

    tests for binary loss function added

--- scripts/loss_functions.py
+++ scripts/loss_functions.py
@@ -1,34 +1,28 @@
 import torch.nn.functional as F
+import torch
+
 
 def multi_class_cross_entropy(output, target):
     """
-    This criterion combines log_softmax and nll_loss in a single function.
+    combines log_softmax and nll_loss in a single function, is used for multiclass classification
     :param output: the input to the loss function is the output as raw, unnormalized scores for each class,
                     is of size (minibatch, C)
     :param target: 1D tensor of size minibatch with range [0,C−1] for each value,
     :return: loss: mean loss of all instances in batch
     """
     assert output.shape[0] == target.shape[0], "target shape is the number of batches in output"
-    assert target.dtype != 'float32', "target type has to be \"int\""
+    assert target.dtype != 'float32', "target type has to be \"int\""  # maybe remove or use regex
     loss = F.cross_entropy(output, target)
     return loss
 
 
-# to be done
 def binary_class_cross_entropy(output, target):
     """
+    applies a sigmoid function plus cross-entropy loss, is used for binary classification
     :param output: the input to the loss function is the output as raw, unnormalized scores for each class
     :param target: tensor of the same shape as input
     :return: loss: mean loss of all instances in batch
     """
-    assert output.shape[0] == target.shape, "target shape is the number of batches in output" # maybe change
-    loss = F.binary_cross_entropy(F.sigmoid(output), target)
+    assert output.shape == target.shape, "target shape is same as output shape"
+    loss = F.binary_cross_entropy(torch.sigmoid(output), target)
     return loss
-
-
-
-
-
-
-
-
--- tests/test_loss_functions.py
+++ tests/test_loss_functions.py
@@ -3,6 +3,7 @@ import torch
 from scripts import multi_class_cross_entropy, binary_class_cross_entropy
 import unittest
 
+
 class LossFunctionsTest(unittest.TestCase):
     """
     this class tests the BasicTwoWordClassifier
@@ -11,15 +12,16 @@ class LossFunctionsTest(unittest.TestCase):
     """
 
     def setUp(self):
-        self.output_tensor = torch.from_numpy(np.array([[5.0, 5.0, 1.0], [0.0, 1.0, 2.0], [1.0,2.0,3.0]],dtype='float32'))
-        self.target_labels = torch.from_numpy(np.array([0,1,2], dtype='long'))
-
-        self.manual_loss = self.calculate_manually(self.output_tensor, self.target_labels)
+        self.output_tensor = torch.from_numpy(
+            np.array([[5.0, 5.0, 1.0], [0.0, 1.0, 2.0], [1.0, 2.0, 3.0]], dtype='float32'))
+        self.target_labels = torch.from_numpy(np.array([0, 1, 2], dtype='long'))
+        self.output_tensor_binary = torch.from_numpy(np.array([0.0, -1.4, -.8, .2, .4, .8, 1.2, 2.2, 2.9, 4.6]))
+        self.target_labels_binary = torch.from_numpy(np.array([1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
 
     @staticmethod
     def calculate_manually(logits, target):
         """
-        Helper method to calculate mean loss manually with the help of numpy
+        Helper method to calculate mean loss manually with the help of numpy (for multiclass loss)
         help from: https://d2l.ai/chapter_linear-networks/softmax-regression-scratch.html and http://cs231n.github.io/neural-networks-case-study/#grad
         """
         logits = logits.numpy()
@@ -27,18 +29,47 @@ class LossFunctionsTest(unittest.TestCase):
         num_examples = logits.shape[0]
 
         exps = [np.exp(i) for i in logits]
-        probs = np.array(exps / np.sum(exps, axis=1, keepdims=True)) # softmax
+        probs = np.array(exps / np.sum(exps, axis=1, keepdims=True))  # softmax
 
-        correct_logprobs = - np.log(probs[range(num_examples), target]) # negative log likelihood
+        correct_logprobs = - np.log(probs[range(num_examples), target])  # negative log likelihood
 
-        return np.sum(correct_logprobs)/num_examples
+        return np.sum(correct_logprobs) / num_examples
 
+    @staticmethod
+    def sigmoid(x):
+        """
+        helper method to calculate sigmoid function
+        """
+        return 1 / (1 + np.exp(-x))
 
+    @staticmethod
+    def cross_entropy(y_hat, y):
+        """
+        helper method to calculate cross entropy
+        """
+        if y == 1:
+            return -np.log(y_hat)
+        else:
+            return -np.log(1 - y_hat)
 
     def test_multiclass_cross_entropy(self):
         """
-        tests whether loss from function corresponds to loss manually calculated
+        multiclass classification: tests whether loss from implemented function corresponds to loss manually calculated
         """
+        expected_loss = self.calculate_manually(self.output_tensor, self.target_labels)
+        loss = multi_class_cross_entropy(output=self.output_tensor, target=self.target_labels)
+        np.testing.assert_allclose(loss, expected_loss)
 
-        loss = multi_class_cross_entropy(self.output_tensor, self.target_labels)
-        np.testing.assert_allclose(loss, self.manual_loss)
+    def test_binary_cross_entropy(self):
+        """
+         binary classification: tests whether loss from implemented function corresponds to loss manually calculated
+         """
+        loss = binary_class_cross_entropy(output=self.output_tensor_binary, target=self.target_labels_binary)
+
+        loss_calc = []
+        for x, y in zip(self.output_tensor_binary, self.target_labels_binary): # apply cross entropy and sigmoid to each element in vector
+            loss_calc.append(self.__cross_entropy(self.__sigmoid(x), y).numpy())
+
+        expected_loss = np.mean(loss_calc)
+
+        np.testing.assert_allclose(loss, expected_loss)
