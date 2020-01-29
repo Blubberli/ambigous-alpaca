ec19c2a9c963fa7656dfc6bafd72bccf7e183198
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-01-29 09:40:46 +0100

    tests and methods included

--- scripts/feature_extractor_static.py
+++ scripts/feature_extractor_static.py
@@ -2,13 +2,32 @@ import finalfusion
 
 
 class StaticEmbeddingExtractor:
+    """
+    this class includes methods to extract pretrained static word embeddings
+    the pretrained embeddings stem from finalfusion
+    :param: path_to_fifu: the path to the pretrained embeddings stored with .fifu format
+    """
 
     def __init__(self, path_to_fifu):
-        self._embeds = finalfusion.Embedding(path_to_fifu, mmap=True)
+        self._embeds = finalfusion.Embeddings(path_to_fifu, mmap=True)
 
     def get_embedding(self, word):
+        """
+        takes a word and returns its embedding
+        :param word: the word for which an embedding should be returned
+        :return: the embedding of the word
+        """
         return self._embeds.embedding(word)
 
+    def get_array_embeddings(self, array_words):
+        """
+        takes an array of words and returns an array of embeddings of those words
+        :param array_words: a word array of length x
+        :return: array_embeddings: the embeddings of those words in an array of length x
+        """
+        array_embeddings = []
+        [array_embeddings.append(self._embeds.embedding(words)) for words in array_words]
+        return array_embeddings
 
     @property
     def embeds(self):
--- tests/test_feature_extractor_static.py
+++ tests/test_feature_extractor_static.py
@@ -1,5 +1,6 @@
 import unittest
 from scripts import StaticEmbeddingExtractor
+import numpy as np
 
 
 class StaticEmbeddingExtractorTest(unittest.TestCase):
@@ -9,13 +10,33 @@ class StaticEmbeddingExtractorTest(unittest.TestCase):
         path_struct = "/Users/ehuber/Documents/ambiguous_alpaca/ambigous-alpaca/german-structgram-mincount-30-ctx-10-dims-300.fifu"
         self.extractor_skipgram = StaticEmbeddingExtractor(path_skipgram)
         self.extractor_structgram = StaticEmbeddingExtractor(path_struct)
+        self.emb_skip = self.extractor_skipgram.get_embedding("Pferd")
+        self.emb_struct = self.extractor_structgram.get_embedding("Pferd")
+        array_words = ["Schaaf", "Pferd", "Hund", "Katze"]
+        self.array_embeddings = self.extractor_skipgram.get_array_embeddings(array_words)
 
     def test_dimension(self):
+        """
+        test dimension of both types of embedding (skipgram and structured skipgrams)
+        """
         expected = 300
-        emb = self.extractor_skipgram.get_embedding("Pferd")
-        #...
-    def test_distance(self):
-        pass
+
+        np.testing.assert_equal(expected, self.emb_skip.shape[0])
+        np.testing.assert_equal(expected, self.emb_struct.shape[0])
+
+    def test_embeddings_diferent(self):
+        """
+        tests that two different embeddings (structured vs. normal skipgram) correspond to the the same word
+        """
+        np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, self.emb_skip, self.emb_struct)
+
+    def test_array_embedding(self):
+        """
+        tests length of array of embeddings and dimension of embedding
+        """
+        expected = 4
+        expected_dimensions = 300
+        np.testing.assert_equal(expected, len(self.array_embeddings))
+        np.testing.assert_equal(expected_dimensions, self.array_embeddings[0].shape[0])
 
 
-    #...
