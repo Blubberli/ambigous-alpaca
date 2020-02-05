aef8bd42041d00ac04513526c640be626d2de77b
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-02-05 16:54:41 +0100

    some logging parts added

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,14 @@ from scripts.loss_functions import multi_class_cross_entropy, binary_class_cross
 import torch
 from torch import optim
 import numpy as np
+import logging
+import logging.config
+from scripts.logger_config import create_config
 
+#from pytorchtools import EarlyStopping
+
+
+# add method that saves predictions to file
 
 def main():
     batch_wordone = torch.from_numpy(
@@ -12,6 +19,13 @@ def main():
             np.array([[1.0, 1.0, 1.0], [1.0, 2.0, 1.0], [2.0, 2.0, 2.0]],dtype="float32"))
     target = torch.from_numpy(
             np.array([0,1,2],dtype="int64"))
+    output_path = "/Users/ehuber/Documents/ambiguous_alpaca/ambigous-alpaca/trylog.txt"
+
+    logging.config.dictConfig(create_config(output_path))
+    logger = logging.getLogger("train")
+    #logging.info().. add information of that was json file
+    logger.info("setting up classifier with shape %s" % batch_wordtwo.shape[1])
+    logger.info("%d training batches" % batch_wordtwo.shape[0]) #....and so on....
     classifier = BasicTwoWordClassifier(input_dim=batch_wordtwo.shape[1] * 2, hidden_dim=batch_wordone.shape[0],
                                         label_nr=len(target))
     optimizer = optim.Adam(classifier.parameters(), lr=0.1)
@@ -22,6 +36,8 @@ def main():
         optimizer.step()
         print(loss)
 if __name__ == "__main__":
+    # read in json file
+
     main()
 
     # read in arguments and save them into a configuration object
