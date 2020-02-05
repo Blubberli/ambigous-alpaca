9275d33d902e136ef543b55b7a5269305ffbef32
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-02-05 16:54:55 +0100

    logger creator added

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,4 +2,6 @@ from scripts.composition_functions import transweigh, transweigh_weight, tw_tran
 from scripts.basic_twoword_classifier import BasicTwoWordClassifier
 from scripts.feature_extractor_contextualized import BertExtractor
 from scripts.loss_functions import multi_class_cross_entropy, binary_class_cross_entropy
+from scripts.logger_config import create_config
+
 
