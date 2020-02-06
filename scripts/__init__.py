186841d45787d5dade49d23e5dd35d3e096bb372
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-02-06 11:56:07 +0100

    init file updated with data loader

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,3 +4,4 @@ from scripts.feature_extractor_contextualized import BertExtractor
 from scripts.loss_functions import multi_class_cross_entropy, binary_class_cross_entropy
 from scripts.feature_extractor_static import StaticEmbeddingExtractor
 from scripts.transweigh_twoword_classifier import TransweighTwoWordClassifier
+from scripts.data_loader import SimplePhraseContextualizedDataset, SimplePhraseStaticDataset
