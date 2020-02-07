e722a5fab7237dc8a08b92427c2f60453dea9d72
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-02-07 09:42:58 +0100

    updated

--- scripts/train.py
+++ scripts/train.py
@@ -10,20 +10,19 @@ import argparse
 import json
 from pathlib import Path
 import time
-from data_loader import SimplePhraseData
-#from pytorchtools import EarlyStopping
+from scripts.data_loader import SimplePhraseContextualizedDataset, SimplePhraseStaticDataset
+from torch.utils.data import DataLoader
+
+from pytorchtools import EarlyStopping
 
 
-from training_utils import init_classifier
+from scripts.training_utils import init_classifier
 # add method that saves predictions to file
 
 
-def train_binary(config):
+def train_binary(config, data):
 
-    model = init_classifier(config)
-    for epoch in range(config["num_epochs"]):
-        out = model()
-    binary_class_cross_entropy()
+
 
     #logger.info("setting up classifier with shape %s" % batch_wordtwo.shape[1])
     #logger.info("%d training batches" % batch_wordtwo.shape[0])  # ....and so on....
@@ -40,8 +39,22 @@ def train_binary(config):
     #     optimizer.step()
     #     print(loss)
 
-def train_multiclass():
-    pass
+def train_multiclass(config, data):
+
+    model = init_classifier(config)
+    optimizer = optim.Adam(model.parameters(), lr=config["trainer"]["lr"]) # or make an if statement for choosing an optimizer
+    train_losses = []
+
+    for epoch in range(config["num_epochs"]):
+        for word1,word2,labels in data:
+            predictions = model(word1, word2)
+            loss = multi_class_cross_entropy(predictions, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+
+
 
 def predict():
     pass
@@ -54,9 +67,11 @@ def do_eval():
     pass
 
 
-def main():
 
-    #instead read json file
+
+
+if __name__ == "__main__":
+    # instead read json file
     # batch_wordone = torch.from_numpy(
     #         np.array([[5.0, 2.0, 3.0], [0.0, 3.0, 1.0], [1.0, 0.0, 6.0]], dtype="float32"))
     # batch_wordtwo = torch.from_numpy(
@@ -67,20 +82,60 @@ def main():
     argp = argparse.ArgumentParser()
     argp.add_argument("path_to_config")
     argp.parse_args()
-    argp.config = json.loads(argp.path_to_config)
+    config = json.loads(argp.path_to_config) # read in arguments and save them into a configuration object
     ts = time.gmtime()
     save_name = format("%s_%s" % (config[""], time.strftime("%Y-%m-%d-%H_%M_%S", ts)))
     path_logfile = str(Path(config["save_path"]).joinpath(save_name + "_log.txt"))
     logging.config.dictConfig(create_config(path_logfile))
     logger = logging.getLogger("train")
-    #logging.info().. add information of that was json file
 
-if __name__ == "__main__":
+    logger.info("Training %s model with %s embeddings. Logging to %s" % (config["model"]["type"], config["feature_extractor"], path_logfile))
+
+    for k,v in config.items():
+        logger.info("%s : %s", (k,v))
+
+    #read in data...
+
+    if config["feature_extractor"]["contextualized_embeddings"] is True:
+        if config["context"] is False:
+            if config["feature_extractor"]["static_embeddings"] is True:
+                dataset = SimplePhraseContextualizedDataset(config["train_data_path"], config["contextualized"]["bert"])
+    else:
+        dataset = SimplePhraseStaticDataset(config["train_data_path"], config["static"]["pretrained_model"])
 
 
-    main()
+    #load data with torch Data Loader
+    train_loader = DataLoader(dataset, batch_size=config["iterator"]["batch_size"], shuffle=True, num_workers=2) # maybe move to train method
+    # load validation data in batches
+    valid_loader = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               sampler=valid_sampler,
+                                               num_workers=0)
 
-    # read in arguments and save them into a configuration object
+    # load test data in batches
+    test_loader = torch.utils.data.DataLoader(test_data,
+                                              batch_size=batch_size,
+                                              num_workers=0)
+
+
+
+    logger.info("%d training batches" % config["iterator"]["batch_size"])
+    logger.info("the training data contains %d words" % len(train_loader))
+
+
+    #train
+    if config["model"]["classification"] == "multi":
+        train_multiclass(config, train_loader, valid_loader)
+    elif config["model"]["classification"] == "binary":
+        train_binary(config, train_loader, valid_loader)
+    else:
+        print("classification has to be specified as either multi or binary")
+
+
+
+
+
+
     # classifier, hidden_layer dropout, path to dataset, hidden dimension, binary classification, no of epochs, gpu, learningrate
     # initialize model, optimizer, create logger
     # training loop for binary classification (+ validation)
