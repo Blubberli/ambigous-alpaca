3b92bde809a3cc25225649e1bc70f23b28c02e54
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-02-20 12:39:25 +0100

    test file set up

---
+++ tests/test_transfer_comp_classifier.py
@@ -0,0 +1,21 @@
+import unittest
+from scripts import TransferCompClassifier
+
+
+
+class StaticEmbeddingExtractorTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def test1(self):
+        """
+        checks whether dimensions are correct
+        :return:
+        """
+        pass
+    def test2(self):
+        """
+        checks whether correct model (correct part of the model) is loaded?
+        :return:
+        """
+        pass
