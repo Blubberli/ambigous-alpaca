57415ea8b339df643d6bdc6a2bf854793b8ca2d8
evva <41426755+evahuber1994@users.noreply.github.com>
2020-03-06 13:42:00 +0100

    Update tunedropout.sh

--- tunedropout.sh
+++ tunedropout.sh
@@ -3,7 +3,7 @@
 CONFIG=/home/neele/PycharmProjects/ambigous-alpaca/scripts/configs/config3_gerco_lstm.json
 TRAIN=/home/neele/PycharmProjects/ambigous-alpaca/scripts/train.py
 
-for i in `seq 2 2 8`;
+for i in `seq 0 2 8`;
  do echo "dropout is now 0.$i";
   json -I -f $CONFIG -e "this.model.dropout='0.$i'";
   python $TRAIN $CONFIG;
