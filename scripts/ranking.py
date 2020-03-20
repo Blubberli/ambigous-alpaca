0cd5ddf2f20bd78d54b10ff38049c0fb3cd76b26
Blubberli <neele.witte@student.uni-tuebingen.de>
2020-03-20 15:11:33 +0100

    wip

--- scripts/ranking.py
+++ scripts/ranking.py
@@ -1,29 +1,33 @@
-#!/usr/bin/env python3
-
 import math
-
 import numpy as np
+import torch
+import torch.nn.functional as F
 from scripts import StaticEmbeddingExtractor
-# import finalfusion
-import ffp
 
 
 class Ranker:
 
-    def __init__(self, path_to_predictions, embedding_path, dataloader, all_labels, phrase_label):
+    def __init__(self, path_to_predictions, path_to_ranks, embedding_path, dataloader, all_labels, max_rank):
         # load composed predictions
         # I have a dataset, e.g. test data. i can iterate through it and for each batch I have:
         # the original phrases
         # I need the matrix of label embeddings and I need to know which embedding belongs to which label
-        self._composed = ffp.embeddings.Embeddings.read(path_to_predictions, "finalfusion")
-        self._composed_embeddings = self._composed.storage
+        self._predicted_embeddings = np.load(path_to_predictions, allow_pickle=True)
         self._embeddings = StaticEmbeddingExtractor(embedding_path)
-        data = next(dataloader)
-        self._true_labels = data[phrase_label]
+        data = next(iter(dataloader))
+        # the correct labels are stored here
+        self._true_labels = data["phrase"]
+        self._max_rank = max_rank
+
+        # construct label embedding matrix
+        all_labels = sorted(all_labels)
         self._label_embeddings = self._embeddings.get_array_embeddings(all_labels)
         self._label2index = dict(zip(all_labels, range(len(all_labels))))
+        # normalize embeddings
+        self._label_embeddings = F.normalize(torch.from_numpy(np.array(self._label_embeddings)), p=2, dim=1)
         self._ranks = self.get_target_based_rank()
-        self.calculate_quartiles(self._ranks)
+        print(self.calculate_quartiles(self._ranks))
+        self.save_ranks(self._ranks, path_to_ranks)
 
     def get_target_based_rank(self):
         """
@@ -35,27 +39,30 @@ class Ranker:
         :param dictionary_embeddings: a gensim model containing the original embeddings
         :return: a list with the ranks for all the composed representations in the batch
         """
+        # self._label_embeddings = F.normalize(torch.from_numpy(np.array(self._label_embeddings)), p=2, dim=1)
+        # self._predicted_embeddings = F.normalize(torch.from_numpy(np.array(self._predicted_embeddings)), p=2, dim=1)
         all_ranks = []
         # get the index for each label in the true labels
         target_idxs = [self._label2index[label] for label in self._true_labels]
         # get a matrix, each row representing the gold representation of the corresponding label
         target_repr = np.take(self._label_embeddings, target_idxs, axis=0)
+
         # get the similarity between each label and each other possible label
+        # result: [labelsize x targetinstances]  = for each instance a vector of cosine similarities to each label
         target_dict_similarities = np.dot(self._label_embeddings, np.transpose(target_repr))
 
-        for i in range(self._composed_embeddings.shape[0]):
+        for i in range(self._predicted_embeddings.shape[0]):
             # compute similarity between the target and the predicted vector
-            target_composed_similarity = np.dot(self._composed_embeddings[i], target_repr[i])
+            target_composed_similarity = np.dot(self._predicted_embeddings[i], target_repr[i])
 
-            # remove the similarity of the target vector to itself
             target_sims = np.delete(target_dict_similarities[:, i], target_idxs[i])
 
             # the rank is the number of vectors with greater similarity that the one between
             # the target representation and the composed one; no sorting is required, just
             # the number of elements that are more similar
             rank = np.count_nonzero(target_sims > target_composed_similarity) + 1
-            # if (rank > max_rank):
-            #    rank = max_rank
+            if (rank > self._max_rank):
+                rank = self._max_rank
             all_ranks.append(rank)
 
         return all_ranks
@@ -75,9 +82,11 @@ class Ranker:
         """
         sorted_data = sorted(ranks)
         leq5 = sum([1 for rank in sorted_data if rank <= 5])
+        leq1 = sum([1 for rank in sorted_data if rank == 1])
         mid_index = math.floor((len(sorted_data) - 1) / 2)
         if len(sorted_data) % 2 != 0:
             quartiles = list(map(np.median, [sorted_data[0:mid_index], sorted_data, sorted_data[mid_index + 1:]]))
         else:
             quartiles = list(map(np.median, [sorted_data[0:mid_index + 1], sorted_data, sorted_data[mid_index + 1:]]))
-        return quartiles, "%.2f%% of ranks <=5" % (100 * leq5 / float(len(sorted_data)))
+        return quartiles, "%.2f of ranks = 1; %.2f%% of ranks <=5" % (
+            (100 * leq1 / float(len(sorted_data))), (100 * leq5 / float(len(sorted_data))))
---
+++ tests/test_ranking.py
@@ -0,0 +1,106 @@
+import unittest
+from pathlib import Path
+import ffp
+import numpy as np
+from scripts.data_loader import extract_all_words, PretrainCompmodelDataset
+from torch.utils.data import DataLoader
+from scripts.ranking import Ranker
+from scripts import StaticEmbeddingExtractor
+
+
+class EvaluationTest(unittest.TestCase):
+    """
+    This class tests the functionality of the evaluation.py script.
+    This test suite can be ran with:
+        python -m unittest -q tests.EvaluationTest
+    """
+
+    def setUp(self):
+        # test_data_dir = Path(__file__).resolve().parents[1]
+        self.predictions = "/Users/nwitte/PycharmProjects/ambigous-alpaca/tests/embeddings/predictions_rank.npy"
+        embedding_path = "embeddings/ranking_embeddings.fifu"
+        self.labels = extract_all_words("data_pretraining/train.txt", "data_pretraining/test.txt",
+                                        "data_pretraining/train.txt", separator=" ", head="head", modifier="modifier",
+                                        phrase="phrase")
+
+        dataset_test = PretrainCompmodelDataset(data_path="data_pretraining/test.txt",
+                                                embedding_path=embedding_path, separator=" ",
+                                                phrase="phrase", mod="modifier", head="head")
+        self.data_loader = DataLoader(dataset=dataset_test, shuffle=False, batch_size=len(dataset_test))
+
+    def test_all_ranks(self):
+        ranker = Ranker(path_to_predictions=self.predictions,
+                        path_to_ranks="data_pretraining/ranks.txt",
+                        embedding_path="embeddings/ranking_embeddings.fifu", all_labels=self.labels,
+                        dataloader=self.data_loader, max_rank=14)
+        ranks = ranker._ranks
+        np.testing.assert_equal(ranks, np.full([8], 1))
+
+    def test_n_nearest(self):
+        ranker = Ranker(path_to_predictions=self.predictions,
+                        path_to_ranks="data_pretraining/ranks.txt",
+                        embedding_path="embeddings/ranking_embeddings.fifu", all_labels=self.labels,
+                        dataloader=self.data_loader, max_rank=5)
+        ranks = ranker._ranks
+        np.testing.assert_equal(ranks, np.full([8], 1))
+
+    def test_bad_prediction(self):
+        ranker = Ranker(path_to_predictions=self.predictions,
+                        path_to_ranks="data_pretraining/ranks.txt",
+                        embedding_path="embeddings/ranking_embeddings.fifu", all_labels=self.labels,
+                        dataloader=self.data_loader, max_rank=5)
+        bad_prediction = np.array([0.0, 1.0])
+        ranker._predicted_embeddings[2] = bad_prediction
+        ranks = ranker.get_target_based_rank()
+        np.testing.assert_equal(ranks[2], 5)
+
+    def test_close_prediction(self):
+        ranker = Ranker(path_to_predictions=self.predictions,
+                        path_to_ranks="data_pretraining/ranks.txt",
+                        embedding_path="embeddings/ranking_embeddings.fifu", all_labels=self.labels,
+                        dataloader=self.data_loader, max_rank=5)
+        close_prediction = np.array([0.65380877, 0.74601357])
+        ranker._predicted_embeddings[2] = close_prediction / np.linalg.norm(close_prediction)
+        ranks = ranker.get_target_based_rank()
+        np.testing.assert_equal(ranks[2], 2)
+
+    def test_ranks(self):
+        predictions = np.array([[0.88143742, 0.47230083], [0.83301994, 0.55324296],
+                                [0.99520118, 0.09784996], [0.50229784, 0.86469467]], dtype=np.float32)
+
+        compounds = ["apfelbaum", "quarkstrudel", "kirschbaum", "kirschstrudel"]
+        correct_ranks = [5, 4, 14, 14]
+        ranker = Ranker(path_to_predictions=self.predictions,
+                        path_to_ranks="data_pretraining/ranks.txt",
+                        embedding_path="embeddings/ranking_embeddings.fifu", all_labels=self.labels,
+                        dataloader=self.data_loader, max_rank=14)
+        ranker._predicted_embeddings = predictions
+        ranker._true_labels = compounds
+        label2index = dict(zip(self.labels, range(len(self.labels))))
+        embedder = StaticEmbeddingExtractor("embeddings/ranking_embeddings.fifu")
+        embeddings = embedder.get_array_embeddings(self.labels)
+        ranks = ranker.get_target_based_rank(label2index=label2index, predicted_embeddings=predictions,
+                                             label_embeddings=embeddings)
+        print("ranks")
+        print(ranks)
+        np.testing.assert_equal(ranks, correct_ranks)
+
+        """
+        def test_assertion(self):
+            self.assertRaises(AssertionError,
+                              lambda: evaluation.get_all_ranks(predictions_file=self._predictions_file,
+                                                               word_embeddings=self._word_embeddings,
+                                                               max_rank=20, batch_size=3, path_to_ranks=""))
+    
+        def test_quartiles_uneven(self):
+            ranks = [6, 7, 15, 36, 39, 40, 41, 42, 43, 47, 49]
+            quartiles, percent = evaluation.calculate_quartiles(ranks)
+            result = [15, 40, 43]
+            np.testing.assert_equal(quartiles, result)
+    
+        def test_quartiles_even(self):
+            ranks = [7, 15, 36, 39, 40, 41]
+            quartiles, percent = evaluation.calculate_quartiles(ranks)
+            result = [15, 37.5, 40]
+            np.testing.assert_equal(quartiles, result)
+"""
