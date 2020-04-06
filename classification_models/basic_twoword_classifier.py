bbde3822b56d901de60ef263c146f7abba5b7e44
Blubberli <neele.witte@student.uni-tuebingen.de>
2020-04-06 16:37:12 +0200

    classification models

---
+++ classification_models/basic_twoword_classifier.py
@@ -0,0 +1,45 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import utils.composition_functions as comp_functions
+
+
+class BasicTwoWordClassifier(nn.Module):
+    """
+    this class includes a basic two-word classifier with one hidden layer and one output layer.
+    :param input_dim: the dimension of the input vector where only embedding size (2 times the size of the embedding of
+    a single word vector) is needed, batch size is implicit
+    :param hidden_dim : the dimension of the hidden layer, batch size is implicit
+    :param label_nr : the dimension of the output layer, i.e. the number of labels
+    """
+
+    def __init__(self, input_dim, hidden_dim, label_nr, dropout_rate):
+        super(BasicTwoWordClassifier, self).__init__()
+        self._hidden_layer = nn.Linear(input_dim, hidden_dim)
+        self._output_layer = nn.Linear(hidden_dim, label_nr)
+        self._dropout_rate = dropout_rate
+
+    def forward(self, batch):
+        """
+        this function takes two words, concatenates them and applies a non-linear matrix transformation (hidden layer)
+        Its output is then fed to an output layer. Then it returns the concatenated and transformed vectors.
+        :param word1: the first word of size batch_size x embedding size
+        :param word2: the first word of size batch_size x embedding size
+        :return: the transformed vectors after output layer
+        """
+        device = batch["device"]
+        word_composed = comp_functions.concat(batch["w1"].to(device), batch["w2"].to(device), axis=1)
+        x = F.relu(self.hidden_layer(word_composed))
+        x = F.dropout(x, p=self.dropout_rate)
+        return self.output_layer(x)
+
+    @property
+    def hidden_layer(self):
+        return self._hidden_layer
+
+    @property
+    def output_layer(self):
+        return self._output_layer
+
+    @property
+    def dropout_rate(self):
+        return self._dropout_rate
---
+++ classification_models/transfer_comp_classifier.py
@@ -0,0 +1,110 @@
+import torch
+from torch import nn
+from torch.functional import F
+from utils.composition_functions import transweigh
+
+
+class TransferCompClassifier(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, label_nr, dropout_rate, normalize_embeddings, pretrained_model,
+                 add_single_words=False):
+        """
+        initialises the transweigh composition model (the transformation tensor and bias as well as the combining
+        tensor and bias) with weights from saved model
+        the weights for the classifier are not loaded from the saved model
+        is based on trasnweigh_twoword_classifier
+        :param input_dim: size of input dimension
+        :param hidden_dim: size of hidden dimension
+        :param label_nr: nr of labels to be predicted
+        :param dropout_rate: how high dropout rate should be
+        :param normalize_embeddings: True if embeddings should be normalized
+        :param pretrained_model: path to saved model
+        """
+        super().__init__()
+        self._pretrained_model = torch.load(pretrained_model)
+
+        self._transformation_tensor = nn.Parameter(self._pretrained_model["_transformation_tensor"], requires_grad=True)
+        self._transformation_bias = nn.Parameter(self._pretrained_model["_transformation_bias"], requires_grad=True)
+        self._combining_tensor = nn.Parameter(self._pretrained_model["_combining_tensor"], requires_grad=True)
+        self._combining_bias = nn.Parameter(self._pretrained_model["_combining_bias"], requires_grad=True)
+
+        self._add_single_words = add_single_words
+        if self.add_single_words:
+            input_dim = input_dim * 3
+        self._hidden = nn.Linear(input_dim, hidden_dim)
+        self._output = nn.Linear(self.hidden.out_features, label_nr)
+        self._dropout_rate = dropout_rate
+        self._normalize_embeddings = normalize_embeddings
+
+    def forward(self, batch):
+        """
+        First composes the input vectors into one representation. This is then feed trough a hidden layer with a Relu
+        and
+        finally trough an output layer that returns weights for each class.
+        :param word1: word1: the representation of the first word (torch tensor)
+        :param word2: word2: the representation of the second word (torch tensor)
+        :param training: training: True if the model should be trained, False if the model is in inference
+        :return: the raw weights for each class
+        """
+        device = batch["device"]
+        self._composed_phrase = self.compose(batch["w1"].to(device), batch["w2"].to(device), self.training)
+        if self.add_single_words:
+            w1_w2 = torch.cat((batch["w1"].to(device), batch["w2"].to(device)), 1)
+            self._composed_phrase = torch.cat((w1_w2, self.composed_phrase), 1)
+        hidden = F.relu(self.hidden(self.composed_phrase))
+        hidden = F.dropout(hidden, p=self.dropout_rate)
+        class_weights = self.output(hidden)
+        return class_weights
+
+    def compose(self, word1, word2, training):
+        composed_phrase = transweigh(word1=word1, word2=word2, transformation_tensor=self.transformation_tensor,
+                                     transformation_bias=self.transformation_bias, combining_bias=self.combining_bias,
+                                     combining_tensor=self.combining_tensor, dropout_rate=self.dropout_rate,
+                                     training=training)
+        if self.normalize_embeddings:
+            composed_phrase = F.normalize(composed_phrase, p=2, dim=1)
+        return composed_phrase
+
+    @property
+    def combining_tensor(self):
+        return self._combining_tensor
+
+    @property
+    def combining_bias(self):
+        return self._combining_bias
+
+    @property
+    def transformation_tensor(self):
+        return self._transformation_tensor
+
+    @property
+    def transformation_bias(self):
+        return self._transformation_bias
+
+    @property
+    def dropout_rate(self):
+        return self._dropout_rate
+
+    @property
+    def normalize_embeddings(self):
+        return self._normalize_embeddings
+
+    @property
+    def hidden(self):
+        return self._hidden
+
+    @property
+    def output(self):
+        return self._output
+
+    @property
+    def composed_phrase(self):
+        return self._composed_phrase
+
+    @property
+    def pretrained_model(self):
+        return self._pretrained_model
+
+    @property
+    def add_single_words(self):
+        return self._add_single_words
