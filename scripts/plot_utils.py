361f443f9077fc06e21baed18a290faf2d5eb305
Blubberli <neele.witte@student.uni-tuebingen.de>
2020-04-21 10:19:15 +0200

    add function to plot for total dataset

--- scripts/plot_utils.py
+++ scripts/plot_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import argparse
@@ -24,7 +25,35 @@ def plot_learning_curves(training_losses, validation_losses, save_path):
     plt.savefig(save_path, dpi=400)
 
 
-def plot_class_distribution(train_labels, save_path, test_labels=None, test_label=None):
+def plot_overall_class_distribution(labels, save_path):
+    """
+    Given a list of labels for one dataset, this method creates a histogram
+    that shows the overall class distribution
+    :param labels: list of labels
+    :param save_path: path to save the plot to.
+    """
+    unique, counts = np.unique(labels, return_counts=True)
+    tup = list(zip(unique, counts))
+    tup.sort(key=lambda tup: tup[1])
+    unique = [t[0] for t in tup]
+    counts = [t[1] for t in tup]
+    plt.bar(unique, counts, color='red', alpha=0.7)
+    max1 = max(counts)
+
+    train_dist = mpatches.Patch(color='red', alpha=0.7, label='classes in dataset')
+
+    plt.legend(handles=[train_dist])
+    plt.title('Class Frequency')
+    plt.xlabel('Class')
+    plt.ylabel('Frequency')
+    plt.xticks(rotation=90, fontsize=5)
+    plt.yticks(np.arange(0, max1, step=25))
+    plt.tight_layout()
+    plt.savefig(save_path, dpi=400)
+    plt.close()
+
+
+def plot_class_distribution_splits(train_labels, save_path, test_labels, test_label):
     """
     Given a list of training labels and a list of validation labels for a dataset, this method creates a histogram
     that shows the class distribution of each split in the same plot. The colors are transparent so that both
@@ -37,23 +66,21 @@ def plot_class_distribution(train_labels, save_path, test_labels=None, test_labe
     unique, counts = np.unique(train_labels, return_counts=True)
     plt.bar(unique, counts, color='red', alpha=0.5)
     max1 = max(counts)
+    unique, counts = np.unique(test_labels, return_counts=True)
+    plt.bar(unique, counts, color='orange', alpha=0.5)
+    max2 = max(counts)
+    train_dist = mpatches.Patch(color='red', alpha=0.5, label='train')
+    test_dist = mpatches.Patch(color='orange', alpha=0.5, label=test_label)
 
-    if test_labels:
-        unique, counts = np.unique(test_labels, return_counts=True)
-        plt.bar(unique, counts, color='orange', alpha=0.5)
-        max2 = max(counts)
-        train_dist = mpatches.Patch(color='red', alpha=0.5, label='train')
-        test_dist = mpatches.Patch(color='orange', alpha=0.5, label=test_label)
-        plt.legend(handles=[train_dist, test_dist])
-        plt.yticks(np.arange(0, max(max1, max2), step=25))
-    else:
-        plt.yticks(np.arange(0, max1, step=25))
+    plt.legend(handles=[train_dist, test_dist])
     plt.title('Class Frequency')
     plt.xlabel('Class')
     plt.ylabel('Frequency')
     plt.xticks(rotation=90, fontsize=5)
+    plt.yticks(np.arange(0, max(max1, max2), step=25))
     plt.tight_layout()
     plt.savefig(save_path, dpi=400)
+    plt.close()
 
 
 if __name__ == '__main__':
@@ -77,14 +104,17 @@ if __name__ == '__main__':
     valid_labels = next(iter(valid_loader))["l"].numpy()
     valid_labels = label_encoder.inverse_transform(valid_labels)
 
-    plot_class_distribution(train_labels=train_labels, test_labels=valid_labels,
-                            save_path=argp.save_path + "_validation.png")
+    plot_class_distribution_splits(train_labels=train_labels, test_labels=valid_labels,
+                                   save_path=argp.save_path + "_validation.png", test_label="validation")
+
+    # load test data in batches
+    test_loader = DataLoader(dataset_test, batch_size=len(dataset_test), shuffle=False)
+    test_labels = next(iter(test_loader))["l"].numpy()
+    test_labels = label_encoder.inverse_transform(test_labels)
+
+    all_labels = list(train_labels) + list(valid_labels) + list(test_labels)
+    plot_overall_class_distribution(all_labels, argp.save_path + "_overall_dataset.png")
 
     if argp.test_data:
-        # load test data in batches
-        test_loader = DataLoader(dataset_test, batch_size=len(dataset_test), shuffle=False)
-        test_labels = next(iter(test_loader))["l"].numpy()
-        test_labels = label_encoder.inverse_transform(test_labels)
-        assert argp.test_path is not None, "please specify a path to save the plot"
-        plot_class_distribution(train_labels=train_labels, test_labels=test_labels,
-                                save_path=argp.save_path + "_test.png")
+        plot_class_distribution_splits(train_labels=train_labels, test_labels=test_labels,
+                                       save_path=argp.save_path + "_test.png", test_label="test")
