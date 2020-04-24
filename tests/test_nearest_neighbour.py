71db7c87de3adcce7d1e156c9c6197974c8edb25
Blubberli <neele.witte@student.uni-tuebingen.de>
2020-04-24 15:07:04 +0200

    give a feature extractor as parameter to the ranker

--- tests/test_nearest_neighbour.py
+++ tests/test_nearest_neighbour.py
@@ -4,6 +4,7 @@ import numpy as np
 from utils.data_loader import extract_all_words, PretrainCompmodelDataset
 from torch.utils.data import DataLoader
 from training_scripts.nearest_neighbour import NearestNeigbourRanker
+from utils import StaticEmbeddingExtractor
 
 
 class RankingTest(unittest.TestCase):
@@ -21,6 +22,7 @@ class RankingTest(unittest.TestCase):
         self.labels = extract_all_words("data_pretraining/test.txt", "data_pretraining/test.txt",
                                         "data_pretraining/test.txt", separator=" ", head="head", modifier="modifier",
                                         phrase="phrase")
+        self.static_extractor = StaticEmbeddingExtractor(self.embedding_path)
 
         dataset_test = PretrainCompmodelDataset(data_path="data_pretraining/test.txt",
                                                 embedding_path=self.embedding_path, separator=" ",
@@ -29,14 +31,14 @@ class RankingTest(unittest.TestCase):
 
     def test_all_ranks(self):
         ranker = NearestNeigbourRanker(path_to_predictions=self.predictions,
-                                       embedding_path=self.embedding_path, all_labels=self.labels,
+                                       embedding_extractor=self.static_extractor, all_labels=self.labels,
                                        data_loader=self.data_loader, max_rank=14, y_label="phrase")
         ranks = ranker._ranks
         np.testing.assert_equal(ranks, np.full([4], 1))
 
     def test_bad_prediction(self):
         ranker = NearestNeigbourRanker(path_to_predictions=self.predictions,
-                                       embedding_path=self.embedding_path, all_labels=self.labels,
+                                       embedding_extractor=self.static_extractor, all_labels=self.labels,
                                        data_loader=self.data_loader, max_rank=4, y_label="phrase")
         bad_prediction = torch.from_numpy(np.array([0.0, 1.0]))
         ranker._predicted_embeddings[2] = bad_prediction
@@ -45,7 +47,7 @@ class RankingTest(unittest.TestCase):
 
     def test_close_prediction(self):
         ranker = NearestNeigbourRanker(path_to_predictions=self.predictions,
-                                       embedding_path=self.embedding_path, all_labels=self.labels,
+                                       embedding_extractor=self.static_extractor, all_labels=self.labels,
                                        data_loader=self.data_loader, max_rank=5, y_label="phrase")
         close_prediction = torch.from_numpy(np.array([0.7030, 0.668]))
         ranker._predicted_embeddings[2] = close_prediction / np.linalg.norm(close_prediction)
@@ -58,7 +60,7 @@ class RankingTest(unittest.TestCase):
 
         correct_ranks = [3, 4, 3, 6]
         ranker = NearestNeigbourRanker(path_to_predictions=self.predictions,
-                                       embedding_path=self.embedding_path,
+                                       embedding_extractor=self.static_extractor,
                                        all_labels=self.labels,
                                        data_loader=self.data_loader, max_rank=6, y_label="phrase")
         ranker._predicted_embeddings = predictions
