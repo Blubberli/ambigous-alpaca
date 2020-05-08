e88064a622662494ef4962f34971ec8cd348c0f6
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-05-08 15:36:33 +0200

    utils updated

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -2,7 +2,7 @@ import torch
 from classification_models import BasicTwoWordClassifier, TransweighTwoWordClassifier, TransferCompClassifier, \
     PhraseContextClassifier, MatrixTwoWordClassifier, MatrixTransferClassifier
 from ranking_models import TransweighPretrain, MatrixPretrain, MatrixTransferRanker, TransweighTransferRanker, \
-    TransweighJointRanker
+    TransweighJointRanker, MatrixJointRanker
 
 from utils import SimplePhraseContextualizedDataset, SimplePhraseStaticDataset, \
     PhraseAndContextDatasetStatic, PhraseAndContextDatasetBert, PretrainCompmodelDataset
@@ -86,6 +86,14 @@ def init_classifier(config):
                                            dropout_rate=config["model"]["dropout"],
                                            normalize_embeddings=config["model"]["normalize_embeddings"],
                                            transformations=config["model"]["transformations"],
+                                           rep1_weight=config["model"]["task_weights"][0],
+                                           rep2_weight=config["model"]["task_weights"][1])
+    if config["model"]["type"] == "joint_ranking_matrix":
+        assert config["model"]["task_weights"][0] + config["model"]["task_weights"][
+            1] == 1.0, "the task weights have to sum to 1"
+        classifier = MatrixJointRanker(input_dim=config["model"]["input_dim"],
+                                           dropout_rate=config["model"]["dropout"],
+                                           normalize_embeddings=config["model"]["normalize_embeddings"],
                                            rep1_weight=config["model"]["task_weights"][0],
                                            rep2_weight=config["model"]["task_weights"][1])
 
